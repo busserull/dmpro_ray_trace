6ee4704588379be1d474a9e0d90642776c555734
Kolbjørn Austreng <kolbjorn.austreng@nordicsemi.no>
2023-09-05 15:00:54 +0200

    6.5: list of hittable objects
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@nordicsemi.no>

--- src/ray.rs
+++ src/ray.rs
@@ -4,6 +4,25 @@ pub trait Hittable {
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord>;
 }
 
+impl<T> Hittable for Vec<T>
+where
+    T: Hittable,
+{
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+        let mut closest_so_far = t_max;
+        let mut record = None;
+
+        for object in self.iter() {
+            if let Some(hit) = object.hit(ray, t_min, closest_so_far) {
+                closest_so_far = hit.t;
+                record = Some(hit);
+            }
+        }
+
+        record
+    }
+}
+
 pub struct HitRecord {
     pub t: f32,
     pub p: Vec3,
